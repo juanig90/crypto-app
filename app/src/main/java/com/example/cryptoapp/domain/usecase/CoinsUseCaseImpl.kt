28583477d2554166155ba136a9d0b8d7927c331c
juan <jr90lp@gmail.com>
2021-04-04 19:31:15 -0300

    Add repository to use case as dependency

--- app/src/main/java/com/example/cryptoapp/domain/usecase/CoinsUseCaseImpl.kt
+++ app/src/main/java/com/example/cryptoapp/domain/usecase/CoinsUseCaseImpl.kt
@@ -1,10 +1,14 @@
 package com.example.cryptoapp.domain.usecase
 
 import com.example.cryptoapp.domain.entity.Coin
+import com.example.cryptoapp.domain.repository.CoinsRepository
+import javax.inject.Inject
 
-class CoinsUseCaseImpl: CoinsUseCase {
+class CoinsUseCaseImpl @Inject constructor(
+        private val repository: CoinsRepository
+) : CoinsUseCase {
 
     override fun getCoins(): List<Coin> {
-        TODO("Not yet implemented")
+        return repository.getCoins()
     }
 }
