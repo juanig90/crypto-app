f9cbeabe40179a4e3369dd74ef7029ad06d7bc6d
juan <jr90lp@gmail.com>
2021-04-10 19:05:20 -0300

    Provide converter to retrofit builder

--- app/src/main/java/com/example/cryptoapp/di/Networking.kt
+++ app/src/main/java/com/example/cryptoapp/di/Networking.kt
@@ -2,7 +2,9 @@ package com.example.cryptoapp.di
 
 import dagger.Module
 import dagger.Provides
+import retrofit2.Converter
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -10,9 +12,17 @@ object Networking {
 
     @Singleton
     @Provides
-    fun providesRetrofit(): Retrofit {
+    fun providesConverter(): Converter.Factory {
+        return MoshiConverterFactory.create()
+    }
+
+
+    @Singleton
+    @Provides
+    fun providesRetrofit(converter: Converter.Factory): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://api.coingecko.com/api/v3")
+            .addConverterFactory(converter)
             .build()
     }
 
