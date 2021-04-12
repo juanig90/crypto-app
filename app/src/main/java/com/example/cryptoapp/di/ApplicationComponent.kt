505bc0e6aa5251daecf1d88217b8a3b74c57e293
juan <jr90lp@gmail.com>
2021-04-11 21:10:25 -0300

    Rename Networking.kt to NetworkingModule.kt

--- app/src/main/java/com/example/cryptoapp/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/cryptoapp/di/ApplicationComponent.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 @Component(
     modules = [
         AppModule::class,
-        Networking::class,
+        NetworkingModule::class,
         Subcomponents::class,
         ViewModelBuilderModule::class,
     ]
