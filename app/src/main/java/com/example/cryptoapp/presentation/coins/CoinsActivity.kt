d3c067acc61dcbb1ca0c55b4d1b8ec7fbe2be708
juan <jr90lp@gmail.com>
2021-04-18 14:12:30 -0300

    Bind activity_coins.xml

--- app/src/main/java/com/example/cryptoapp/presentation/coins/CoinsActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/coins/CoinsActivity.kt
@@ -3,10 +3,11 @@ package com.example.cryptoapp.presentation.coins
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
 import com.example.cryptoapp.CryptoApp
 import com.example.cryptoapp.R
+import com.example.cryptoapp.databinding.ActivityCoinsBinding
 import javax.inject.Inject
 
 class CoinsActivity : AppCompatActivity() {
@@ -15,16 +16,15 @@ class CoinsActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<CoinsViewModel> { viewModelFactory }
 
-    private lateinit var recyclerCoins: RecyclerView
+    private lateinit var binding: ActivityCoinsBinding
 
     private val coinAdapter = CoinsAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (application as CryptoApp).application.coinsComponent().create().inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coins)
-        recyclerCoins = findViewById(R.id.activity_coins_recycler)
-        recyclerCoins.adapter = coinAdapter
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_coins)
+        binding.adapter = coinAdapter
         viewModel.liveCoins.observe(this, { coins ->
             coinAdapter.coins = coins
         })
