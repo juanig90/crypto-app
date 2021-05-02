3f64a19c600abb2d1caf76da44d7988e6dcdc49c
juan <jr90lp@gmail.com>
2021-05-02 15:41:45 -0300

    Implement save and delete coin on use case

--- app/src/main/java/com/example/cryptoapp/domain/usecase/CoinsUseCase.kt
+++ app/src/main/java/com/example/cryptoapp/domain/usecase/CoinsUseCase.kt
@@ -1,9 +1,12 @@
 package com.example.cryptoapp.domain.usecase
 
 import com.example.cryptoapp.domain.entity.Coin
+import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 
 interface CoinsUseCase {
 
     fun getCoins(): Single<List<Coin>>
+    fun saveCoin(coin: Coin): Completable
+    fun deleteCoin(coin: Coin): Completable
 }
--- app/src/main/java/com/example/cryptoapp/domain/usecase/CoinsUseCaseImpl.kt
+++ app/src/main/java/com/example/cryptoapp/domain/usecase/CoinsUseCaseImpl.kt
@@ -3,6 +3,7 @@ package com.example.cryptoapp.domain.usecase
 import com.example.cryptoapp.domain.entity.Coin
 import com.example.cryptoapp.domain.repository.CoinsRepository
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
@@ -11,6 +12,18 @@ class CoinsUseCaseImpl @Inject constructor(private val repository: CoinsReposito
 
     override fun getCoins(): Single<List<Coin>> {
         return repository.getCoins()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    override fun saveCoin(coin: Coin): Completable {
+        return repository.saveCoin(coin)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    override fun deleteCoin(coin: Coin): Completable {
+        return repository.deleteCoin(coin)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
     }
