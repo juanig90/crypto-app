a1114da0cd4ed94581d60382da50f44e0f391fe7
juan <jr90lp@gmail.com>
2021-05-15 16:28:20 -0300

    Extract code to private function

--- app/src/main/java/com/example/cryptoapp/data/CoinsRepositoryImpl.kt
+++ app/src/main/java/com/example/cryptoapp/data/CoinsRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.example.cryptoapp.data.entity.LocalCoin
 import com.example.cryptoapp.data.entity.RemoteCoin
 import com.example.cryptoapp.domain.entity.Coin
 import com.example.cryptoapp.domain.repository.CoinsRepository
+import io.reactivex.rxjava3.annotations.NonNull
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 
@@ -12,26 +13,31 @@ class CoinsRepositoryImpl(
     private val remoteData: RemoteDataSource): CoinsRepository {
 
     override fun getCoins(local: Boolean): Single<List<Coin>> {
-        if (local)
-            return localData.getCoins().map { coins ->
-                coins.map { localCoin ->
-                    Coin(
-                        id = localCoin.id,
-                        symbol = localCoin.symbol,
-                        name = localCoin.name
-                    )
-                }
+        return if (local) getLocalCoins() else getRemoteCoins()
+    }
+
+    private fun getRemoteCoins(): @NonNull Single<List<Coin>> {
+        return remoteData.getCoins().map { coins ->
+            coins.map { coin ->
+                Coin(
+                    id = coin.id,
+                    symbol = coin.symbol,
+                    name = coin.name
+                )
             }
-        else
-            return remoteData.getCoins().map { coins ->
-                coins.map { coin ->
-                    Coin(
-                        id = coin.id,
-                        symbol = coin.symbol,
-                        name = coin.name
-                    )
-                }
+        }
+    }
+
+    private fun getLocalCoins(): Single<List<Coin>> {
+        return localData.getCoins().map { coins ->
+            coins.map { localCoin ->
+                Coin(
+                    id = localCoin.id,
+                    symbol = localCoin.symbol,
+                    name = localCoin.name
+                )
             }
+        }
     }
 
     override fun saveCoin(coin: Coin): Completable {
