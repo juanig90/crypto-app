e9a4c55e287d0d44acf45013ac825c5edaf05722
juan <jr90lp@gmail.com>
2021-05-15 16:37:59 -0300

    Add attribute to entity

--- app/src/main/java/com/example/cryptoapp/domain/entity/Coin.kt
+++ app/src/main/java/com/example/cryptoapp/domain/entity/Coin.kt
@@ -3,5 +3,5 @@ package com.example.cryptoapp.domain.entity
 data class Coin(
         val id: String,
         val symbol: String,
-        val name: String
-)
+        val name: String,
+        val isFavorite: Boolean = false)
