d8c8db32e01dbcf8cbb3924c2a4d979e19c4670a
juan <jr90lp@gmail.com>
2021-05-23 13:13:48 -0300

    Add empty DetailActivity

---
+++ app/src/main/java/com/example/cryptoapp/presentation/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cryptoapp.presentation.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cryptoapp.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
