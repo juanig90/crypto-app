f0cb6ee6e89953fa0cbeaa62faccfa1cf31991ec
juan <jr90lp@gmail.com>
2021-05-23 15:52:25 -0300

    Add coin detail endpoint

--- app/src/main/java/com/example/cryptoapp/data/remote/CoinAPI.kt
+++ app/src/main/java/com/example/cryptoapp/data/remote/CoinAPI.kt
@@ -1,11 +1,16 @@
 package com.example.cryptoapp.data.remote
 
 import com.example.cryptoapp.data.entity.RemoteCoin
+import com.example.cryptoapp.data.entity.RemoteCoinDetail
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CoinAPI {
 
     @GET("coins/list")
     fun getCoinsList(): Single<List<RemoteCoin>>
+
+    @GET("coins/{id}")
+    fun getCoinDetail(@Path("id") id: Int): Single<List<RemoteCoinDetail>>
 }
