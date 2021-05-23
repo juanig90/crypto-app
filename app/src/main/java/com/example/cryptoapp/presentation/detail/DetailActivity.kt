0ed1ea737e474ea9bb268fc4f15bedc9c00cb0c7
juan <jr90lp@gmail.com>
2021-05-23 17:47:18 -0300

    Get coin detail on create activity

--- app/src/main/java/com/example/cryptoapp/presentation/detail/DetailActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/detail/DetailActivity.kt
@@ -1,11 +1,13 @@
 package com.example.cryptoapp.presentation.detail
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.cryptoapp.CryptoApp
 import com.example.cryptoapp.R
+import java.lang.IllegalArgumentException
 import javax.inject.Inject
 
 class DetailActivity : AppCompatActivity() {
@@ -19,5 +21,17 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         (application as CryptoApp).application.detailComponent().create().inject(this)
+        val id = intent.extras?.getString(COIN_ID_EXTRA) ?: throw IllegalArgumentException("Expected coin id")
+        viewModel.apply {
+            liveData.observe(this@DetailActivity, {
+                Log.d(TAG, "Observe Data:$it ")
+            })
+            getDetail(id)
+        }
+    }
+
+    companion object {
+        const val COIN_ID_EXTRA = "COIN_ID_EXTRA"
+        private const val TAG = "DetailActivity"
     }
 }
--- app/src/main/java/com/example/cryptoapp/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/detail/DetailViewModel.kt
@@ -1,8 +1,35 @@
 package com.example.cryptoapp.presentation.detail
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.cryptoapp.domain.entity.CoinDetail
 import com.example.cryptoapp.domain.usecase.CoinsUseCase
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
 import javax.inject.Inject
 
 class DetailViewModel @Inject constructor(private val useCase: CoinsUseCase): ViewModel() {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    private val mutableLiveData = MutableLiveData<CoinDetail>()
+
+    val liveData: LiveData<CoinDetail> = mutableLiveData
+
+    fun getDetail(id: String) {
+        useCase.getCoinDetail(id).subscribe({ detail ->
+            Log.d("DetailViewModel", "getDetail: $detail")
+            mutableLiveData.value = detail
+        },{
+            Log.e("DetailViewModel", "onError:${it.message}")
+        }).addTo(compositeDisposable)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+
 }
