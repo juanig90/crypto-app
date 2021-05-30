1f0462f336069d7d664665b77e836c40a7a836cf
juan <jr90lp@gmail.com>
2021-05-30 17:52:01 -0300

    Add extension function to show snackbar

---
+++ app/src/main/java/com/example/cryptoapp/domain/extension/Activity.kt
@@ -0,0 +1,15 @@
+package com.example.cryptoapp.domain.extension
+
+import android.app.Activity
+import android.view.View
+import com.example.cryptoapp.R
+import com.google.android.material.snackbar.Snackbar
+
+fun Activity.showSnackbar(view: View, msg: String, onRetry: View.OnClickListener) {
+    val snackbar = Snackbar.make(view, msg, Snackbar.LENGTH_INDEFINITE)
+    snackbar.run {
+        setAction(view.context.getString(R.string.retry), onRetry)
+        show()
+    }
+
+}
