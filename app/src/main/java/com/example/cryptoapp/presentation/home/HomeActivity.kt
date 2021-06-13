7ba458510ae79ab26aa76d896edd9035a8458543
juan <jr90lp@gmail.com>
2021-06-13 17:45:52 -0300

    Show use case for first install

--- app/src/main/java/com/example/cryptoapp/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/home/HomeActivity.kt
@@ -1,8 +1,10 @@
 package com.example.cryptoapp.presentation.home
 
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -14,6 +16,8 @@ import com.example.cryptoapp.presentation.CoinsViewModel
 import com.example.cryptoapp.presentation.GridItemDecoration
 import com.example.cryptoapp.presentation.coins.CoinsActivity
 import com.example.cryptoapp.presentation.detail.DetailActivity
+import com.getkeepsafe.taptargetview.TapTarget
+import com.getkeepsafe.taptargetview.TapTargetView
 import javax.inject.Inject
 
 class HomeActivity : AppCompatActivity() {
@@ -46,11 +50,50 @@ class HomeActivity : AppCompatActivity() {
                 })
             }
         }
+        showUseCase()
     }
 
     override fun onResume() {
         super.onResume()
         viewModel.onLoadCoins(true)
+    }
+
+    private fun showUseCase() {
+        val sharedPreferences = getSharedPreferences("crypto_app", MODE_PRIVATE)
+        val isFirstUse = sharedPreferences.getBoolean(FIRST_USE, true)
+        if (isFirstUse) {
+            TapTargetView.showFor(
+                this,
+                TapTarget.forView(
+                    binding.activityHomeFloatingButton, getString(R.string.title_show_case),
+                    getString(R.string.show_case)
+                )
+                    .tintTarget(false)
+                    .outerCircleColor(R.color.purple_500)
+                    .outerCircleAlpha(0.96f)
+                    .targetCircleColor(R.color.white)
+                    .titleTextSize(20)
+                    .titleTextColor(R.color.white)
+                    .descriptionTextSize(10)
+                    .textColor(R.color.switch_thumb_normal_material_light)
+                    .textTypeface(Typeface.SANS_SERIF)
+                    .dimColor(R.color.purple_500)
+                    .drawShadow(true)
+                    .cancelable(true)
+                    .transparentTarget(false)
+                    .targetRadius(60),
+                object : TapTargetView.Listener() {
+                    override fun onTargetClick(view: TapTargetView?) {
+                        super.onTargetClick(view)
+                        binding.activityHomeFloatingButton.performClick()
+                    }
+                })
+            sharedPreferences.edit(commit = true) { putBoolean(FIRST_USE, false) }
+        }
+    }
+
+    companion object {
+        private const val FIRST_USE = "First use"
     }
 
 }
