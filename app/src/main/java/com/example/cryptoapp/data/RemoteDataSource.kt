07f157cb91d470d112c314b1be03503acd162830
juan <jr90lp@gmail.com>
2021-06-12 21:30:54 -0300

    New method to access to historical prices on RemoteDataSource

--- app/src/main/java/com/example/cryptoapp/data/RemoteDataSource.kt
+++ app/src/main/java/com/example/cryptoapp/data/RemoteDataSource.kt
@@ -2,10 +2,12 @@ package com.example.cryptoapp.data
 
 import com.example.cryptoapp.data.entity.RemoteCoin
 import com.example.cryptoapp.data.entity.RemoteCoinDetail
+import com.example.cryptoapp.data.entity.RemoteHistoricalPrices
 import io.reactivex.rxjava3.core.Single
 
 interface RemoteDataSource {
 
     fun getCoins(): Single<List<RemoteCoin>>
     fun getDetailCoin(id: String): Single<RemoteCoinDetail>
+    fun getHistoricalPrices(id: String): Single<RemoteHistoricalPrices>
 }
--- app/src/main/java/com/example/cryptoapp/data/remote/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/cryptoapp/data/remote/RemoteDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.example.cryptoapp.data.remote
 import com.example.cryptoapp.data.RemoteDataSource
 import com.example.cryptoapp.data.entity.RemoteCoin
 import com.example.cryptoapp.data.entity.RemoteCoinDetail
+import com.example.cryptoapp.data.entity.RemoteHistoricalPrices
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
@@ -10,4 +11,5 @@ class RemoteDataSourceImpl @Inject constructor(private val client: CoinAPI): Rem
 
     override fun getCoins(): Single<List<RemoteCoin>> = client.getCoinsList()
     override fun getDetailCoin(id: String): Single<RemoteCoinDetail> = client.getCoinDetail(id)
+    override fun getHistoricalPrices(id: String): Single<RemoteHistoricalPrices> = client.getHistoricalCoin(id)
 }
