bbb934056ce8c2cfa15c1210183609ac282a6e67
juan <jr90lp@gmail.com>
2021-06-24 15:18:27 -0300

    Remove binding methods

--- app/src/main/java/com/example/cryptoapp/presentation/ViewBindings.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/ViewBindings.kt
@@ -1,19 +1,11 @@
 package com.example.cryptoapp.presentation
 
-import android.content.Context
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.cryptoapp.R
-import com.example.cryptoapp.domain.extension.format
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.Description
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
 
 object ViewBindings {
 
@@ -21,21 +13,6 @@ object ViewBindings {
     @BindingAdapter("app:visibility")
     fun bindVisibility(view: View, value: Boolean) {
         if(value) view.visibility = VISIBLE else view.visibility = GONE
-    }
-
-    @JvmStatic
-    @BindingAdapter("app:format")
-    fun bindFormat(textView: TextView, value: Float?) {
-        if (value == null) return
-        val text = formatFloat(value, textView.context, true)
-        textView.text = text
-    }
-
-    @JvmStatic
-    @BindingAdapter("app:simple_format")
-    fun bindSimpleFormat(textView: TextView, value: Float) {
-        val text = formatFloat(value, textView.context, false)
-        textView.text = text
     }
 
     @JvmStatic
@@ -47,13 +24,5 @@ object ViewBindings {
             imageView.setImageResource(R.drawable.ic_expand_less_black)
     }
 
-    private fun formatFloat(value: Float,
-                            context: Context,
-                            includePercentageSymbol: Boolean = false): String {
-        return if(includePercentageSymbol)
-            context.getString(R.string.percentage_value, value.format(2))
-        else
-            value.format(2)
-    }
 
 }
