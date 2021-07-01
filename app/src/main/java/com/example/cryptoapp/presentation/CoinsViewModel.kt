13a4a6dfb863ab995e972d72ca5dfbe3020a5d21
juan <jr90lp@gmail.com>
2021-07-01 20:14:13 -0300

    Collect flow on view model

--- app/src/main/java/com/example/cryptoapp/presentation/CoinsViewModel.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinsViewModel.kt
@@ -1,13 +1,15 @@
 package com.example.cryptoapp.presentation
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.cryptoapp.data.Result
 import com.example.cryptoapp.domain.entity.Coin
 import com.example.cryptoapp.domain.usecase.CoinsUseCase
-import com.example.cryptoapp.presentation.CoinsViewModel.CoinUI.*
+import com.example.cryptoapp.presentation.CoinsViewModel.CoinUI.SwitchUI
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 private const val TAG = "CoinsViewModel"
@@ -35,7 +37,14 @@ class CoinsViewModel @Inject constructor(private val coinsUseCase: CoinsUseCase)
     private val mutableLiveCoin = MutableLiveData<Coin>()
 
     fun onLoadCoins(local: Boolean = false) {
-        TODO("Not yet implemented")
+        viewModelScope.launch {
+            coinsUseCase.getCoins().collect { result ->
+                when(result) {
+                    is Result.Success -> mutableLiveCoins.value = SwitchUI(result.data)
+                    is Result.Error -> mutableLiveError.value = result.msg
+                }
+            }
+        }
     }
 
     fun onSwitchChanged(coin: Coin, value: Boolean) {
