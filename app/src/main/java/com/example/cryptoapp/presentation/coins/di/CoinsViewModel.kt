689e764436dccdada2db585a32d7bd5f4a53d0f9
juan <jr90lp@gmail.com>
2021-07-04 19:04:36 -0300

    Remove unused methods and vars on CoinsViewModel

--- app/src/main/java/com/example/cryptoapp/presentation/coins/di/CoinsViewModel.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/coins/di/CoinsViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.cryptoapp.data.Result
 import com.example.cryptoapp.domain.entity.OptionItemUI
-import com.example.cryptoapp.domain.entity.FavoriteItemUI
 import com.example.cryptoapp.domain.usecase.CoinsUseCase
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.addTo
@@ -21,12 +20,6 @@ class CoinsViewModel @Inject constructor(private val coinsUseCase: CoinsUseCase)
     val liveCoins: LiveData<List<OptionItemUI>>
         get() = mutableLiveCoins
 
-    val liveFavorites: LiveData<List<FavoriteItemUI>>
-        get() = mutableLiveFavorites
-
-    val liveCoinSelected: LiveData<String>
-        get() = mutableLiveCoinSelected
-
     val liveLoading: LiveData<Boolean>
         get() = mutableLiveLoading
 
@@ -34,10 +27,6 @@ class CoinsViewModel @Inject constructor(private val coinsUseCase: CoinsUseCase)
         get() = mutableLiveError
 
     private val mutableLiveCoins = MutableLiveData<List<OptionItemUI>>()
-
-    private val mutableLiveFavorites = MutableLiveData<List<FavoriteItemUI>>()
-
-    private val mutableLiveCoinSelected = MutableLiveData<String>()
 
     private val mutableLiveLoading = MutableLiveData<Boolean>()
 
@@ -63,20 +52,6 @@ class CoinsViewModel @Inject constructor(private val coinsUseCase: CoinsUseCase)
             }).addTo(compositeDisposable)
     }
 
-    fun onLoadFavorites() {
-        coinsUseCase.getFavoriteItems()
-            .doOnSubscribe { mutableLiveLoading.value = true }
-            .doOnTerminate { mutableLiveLoading.value = false }
-            .subscribe({ result ->
-                when(result) {
-                    is Result.Success -> mutableLiveFavorites.value = result.data
-                    is Result.Error -> mutableLiveError.value = result.msg
-                }
-            }, {
-                Log.e(TAG, "onLoadFavorites:onError ${it.localizedMessage}")
-            }).addTo(compositeDisposable)
-    }
-
     fun onSwitchChanged(item: OptionItemUI, value: Boolean) {
         if (value)
             coinsUseCase.saveFavorite(item).subscribe({
@@ -90,10 +65,6 @@ class CoinsViewModel @Inject constructor(private val coinsUseCase: CoinsUseCase)
             }, {
                 Log.e(TAG, "onSwitchChanged delete error ${it.localizedMessage}")
             }).addTo(compositeDisposable)
-    }
-
-    fun onCoinSelected(id: String) {
-        mutableLiveCoinSelected.value = id
     }
 
 }
