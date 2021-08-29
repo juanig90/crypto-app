d34511201b48b45ecdcd7f68dc786298ede101ad
juan <jr90lp@gmail.com>
2021-08-29 20:06:29 -0300

    Handle ConnectivityException

--- app/src/main/java/com/example/cryptoapp/domain/ErrorMapperImpl.kt
+++ app/src/main/java/com/example/cryptoapp/domain/ErrorMapperImpl.kt
@@ -2,13 +2,13 @@ package com.example.cryptoapp.domain
 
 import android.content.Context
 import com.example.cryptoapp.R
-import java.net.UnknownHostException
+import com.example.cryptoapp.domain.exceptions.ConnectivityException
 
 class ErrorMapperImpl(private val applicationContext: Context): ErrorMapper {
 
     override fun mapError(throwable: Throwable): String {
             return when(throwable) {
-                is UnknownHostException -> applicationContext.getString(R.string.check_your_connection)
+                is ConnectivityException -> applicationContext.getString(R.string.check_your_connection)
                 else -> applicationContext.getString(R.string.unknown_error)
         }
     }
