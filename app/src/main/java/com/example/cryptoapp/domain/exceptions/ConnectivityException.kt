2e3042b41132c59d94b407f7c26850f5bc900a71
Juan <jr90lp@gmail.com>
2022-03-27 16:21:34 -0300

    Handle new exceptions

--- app/src/main/java/com/example/cryptoapp/domain/exceptions/ConnectivityException.kt
+++ app/src/main/java/com/example/cryptoapp/domain/exceptions/ConnectivityException.kt
@@ -3,3 +3,9 @@ package com.example.cryptoapp.domain.exceptions
 import java.io.IOException
 
 class ConnectivityException: IOException("No Internet Connection")
+
+class ClientError(msg: String): IOException(msg)
+
+class ServerError(msg: String): IOException(msg)
+
+class UnknownRequestError(msg: String): IOException(msg)
