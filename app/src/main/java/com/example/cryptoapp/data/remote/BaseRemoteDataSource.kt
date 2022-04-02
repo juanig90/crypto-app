eb11c7a1fb66ffd0bbf729b09690a13fa0514d74
Juan <jr90lp@gmail.com>
2022-04-02 20:41:43 -0300

    Add BaseRemoteDataSource

---
+++ app/src/main/java/com/example/cryptoapp/data/remote/BaseRemoteDataSource.kt
@@ -0,0 +1,23 @@
+package com.example.cryptoapp.data.remote
+
+import com.example.cryptoapp.domain.exceptions.ClientError
+import com.example.cryptoapp.domain.exceptions.ServerError
+import com.example.cryptoapp.domain.exceptions.UnknownRequestError
+import retrofit2.HttpException
+
+open class BaseRemoteDataSource {
+
+    open suspend fun <T> runCatch(block: suspend() -> T): T {
+        return try {
+            block()
+        }
+        catch (e: HttpException) {
+            val exception = when(e.code()) {
+                in 400..499 -> ClientError(e.message())
+                in 500..599 -> ServerError(e.message())
+                else -> UnknownRequestError(e.message())
+            }
+            throw exception
+        }
+    }
+}
